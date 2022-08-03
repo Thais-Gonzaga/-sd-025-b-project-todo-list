58346e0e08741990ff6272caae9d8f56ff11e630
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-03 14:09:43 -0300

    adiciona requisito 3 e 4

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const p = document.createElement('p');
 p.id = 'funcionamento';
 p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 header.appendChild(p);
+
+const divInputButton = document.createElement('div');
+document.body.appendChild(divInputButton);
+
+const input = document.createElement('input');
+input.id = 'texto-tarefa';
+divInputButton.appendChild(input);
+
+const ol = document.createElement('ol');
+ol.id = 'lista-tarefas';
+document.body.appendChild(ol);
